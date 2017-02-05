2fbf1cbf667f4b7c4fd06a458c0498dde14f7f8f
chenquxp <1926371@qq.com>
2017-02-05 21:26:14 +0800

    Add backkey Dialog.

--- app/src/main/java/com/chenqu/toolbox/easysoney/EasySoneyActivity.java
+++ app/src/main/java/com/chenqu/toolbox/easysoney/EasySoneyActivity.java
@@ -100,6 +100,7 @@ public class EasySoneyActivity extends AppCompatActivity implements View.OnClick
                 break;
             case R.id.exit_item:
                 Toast.makeText(this, "you clicked exit", Toast.LENGTH_SHORT).show();
+                timer.cancel();
                 finish();
                 break;
             default:
@@ -182,7 +183,46 @@ public class EasySoneyActivity extends AppCompatActivity implements View.OnClick
         timer.cancel();
         super.onDestroy();
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK )
+        {
+            // 创建退出对话框
+            AlertDialog isExit = new AlertDialog.Builder(this).create();
+            // 设置对话框标题
+            isExit.setTitle("系统提示");
+            // 设置对话框消息
+            isExit.setMessage("确定要退出吗,点击取消转后台运行");
+            // 添加选择按钮并注册监听
+            isExit.setButton("确定", listener);
+            isExit.setButton2("取消", listener);
+            // 显示对话框
+            isExit.show();
 
+        }
+
+        return false;
+
+    }
+    /**监听对话框里面的button点击事件*/
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener()
+    {
+        public void onClick(DialogInterface dialog, int which)
+        {
+            switch (which)
+            {
+                case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序
+                    finish();
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框
+                    moveTaskToBack(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     Handler handler = new Handler() {
         @Override
